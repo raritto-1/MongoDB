554db140bdd84dbc45d55da62746fd1256ef3388
raritto-1 <mohitmeena18803@gmail.com>
2024-06-03 16:38:24 +0530

    finding_data_in_database

---
+++ finding_data.py
@@ -0,0 +1,19 @@
+import pymongo
+
+if __name__ == "__main__":
+    print("welcome to mongodb")
+
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    print(client)
+    db = client["lalit"]
+    connection = db["myconnection"]
+
+    # #find_one
+    # one = connection.find_one({"_id": 1})
+    # print(one)
+
+    #find_all
+
+    all_doc = connection.find({"Location": "Jaipur"})
+    for i in all_doc:
+        print(i)  
